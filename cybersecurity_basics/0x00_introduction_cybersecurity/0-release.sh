bbbe7b5ac47df309767acae2477c0c06905a9f19
Florian ROUXEL <f.rouxel@groupeonepoint.com>
2024-04-16 12:02:01 +1000

    Release

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#echo $(lsb_release -a | grep -i 'distributor ID' | sed -ne 's/.*\:\s*\(.*\)/\1/p')
-function rel {
-	return "$(lsb_release -a | grep -i 'distributor ID' | sed -ne 's/.*\:\s*\(.*\)/\1/p')"
-}
+function rel { return "$(lsb_release -a | grep -i 'distributor ID' | sed -ne 's/.*\:\s*\(.*\)/\1/p')" }
 
