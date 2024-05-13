935706b13f655c18180e600c7ef9cec62e756c61
Florian ROUXEL <f.rouxel@groupeonepoint.com>
2024-05-13 14:18:15 +1000

    Add network_security

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | egrep -i '^(registrant|admin|tech)' | awk -F':' '{print $1","$2}' > $1.csv
+whois $1 | egrep '^(Registrant|Admin|Tech)' | awk -F':' '{print $1","$2}' > $1.csv
